ac9f2d1720c402d06390e6b1ca6fbb5843871790
ljh0608 <m98m@catholic.ac.kr>
2023-04-18 21:18:11 +0900

    Refactor: renderBooks함수와 filterBooks함수 분리

--- assignment3/bookstore.js
+++ assignment3/bookstore.js
@@ -2,9 +2,19 @@ import BookInfo from './BookInfo.js';
 
 const bookList = document.querySelector('.section_ul');
 
-// const renderBooks=()=>{
-
-// }
+const renderBooks = (filterBook) => {
+    filterBook.forEach((book) => {
+        const bookItem = document.createElement('li');
+        bookItem.classList.add('section_book', 'pulse');
+        bookItem.innerHTML = `
+    <header class="section_book_header"><h2>${book.title}</h2></header>
+    <p>#${book.hashtag}</p>
+    <img class="book_img"src="${book.image}" alt="${book.title}이미지">
+    <i class="fas fa-solid fa-heart"></i>
+  `;
+        bookList.appendChild(bookItem);
+    });
+};
 
 const filterBooks = (category) => {
     while (bookList.firstChild) {
@@ -18,18 +28,7 @@ const filterBooks = (category) => {
     if (category[0] === '전체') {
         console.log('전체일때 실행');
         filterBook = BookInfo;
-
-        filterBook.forEach((book) => {
-            const bookItem = document.createElement('li');
-            bookItem.classList.add('section_book', 'pulse');
-            bookItem.innerHTML = `
-        <header class="section_book_header"><h2>${book.title}</h2></header>
-        <p>#${book.hashtag}</p>
-        <img class="book_img"src="${book.image}" alt="${book.title}이미지">
-        <i class="fas fa-solid fa-heart"></i>
-      `;
-            bookList.appendChild(bookItem);
-        });
+        renderBooks(filterBook);
     } else if (category === ' ') {
         console.log('render 2');
         while (bookList.firstChild) {
@@ -40,17 +39,7 @@ const filterBooks = (category) => {
         console.log(category.length);
         for (let i = 0; i < category.length; i++) {
             filterBook = BookInfo.filter((book) => book.category === category[i]);
-            filterBook.forEach((book) => {
-                const bookItem = document.createElement('li');
-                bookItem.classList.add('section_book', 'pulse');
-                bookItem.innerHTML = `
-            <header class="section_book_header"><h2>${book.title}</h2></header>
-            <p>#${book.hashtag}</p>
-            <img class="book_img"src="${book.image}" alt="${book.title}이미지">
-            <i class="fas fa-solid fa-heart"></i>
-          `;
-                bookList.appendChild(bookItem);
-            });
+            renderBooks(filterBook);
         }
     }
 };
@@ -65,28 +54,12 @@ checkboxes.forEach((checkbox) => {
             if (checkbox.checked) {
                 checkedCategory.push(checkbox.getAttribute('id'));
                 console.log('체크됨!' + checkbox.getAttribute('id'));
-            } else {
-                console.log(checkedCategory);
-                const index = checkedCategory.indexOf(checkbox.getAttribute('id'));
-                console.log('체크해제~~');
-
-                console.log(index);
-                if (index !== -1) {
-                    checkedCategory.splice(index, 1);
-                    console.log('체크해제@@' + checkbox.getAttribute('id'));
-                    console.log(checkedCategory);
-                }
             }
         });
 
         if (checkedCategory.length === 0) {
-            console.log('render불러오나?');
             filterBooks(' ');
         } else {
-            // checkedCategory.forEach((category) => {
-            //     console.log('마지막 카테고리 확인' + category);
-            //     renderBooks(category);
-            // });
             filterBooks(checkedCategory);
         }
     });
