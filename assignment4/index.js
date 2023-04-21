943589bc29a8930eb4430a4767165279effcf682
ljh0608 <m98m@catholic.ac.kr>
2023-04-21 20:59:42 +0900

    Feat: 모달생성함수 분리 / modal close, add 구현

--- assignment4/index.js
+++ assignment4/index.js
@@ -1,5 +1,6 @@
 import todolists from './Todolist.js';
 
+//todoList의 개수를 세는 함수
 const countTodolist = () => {
     let sum = 0;
     todolists.forEach((todoL) => {
@@ -8,6 +9,7 @@ const countTodolist = () => {
     return sum;
 };
 
+//footer 버튼 click event
 const calendarBtn = document.querySelector('.calendar_btn');
 calendarBtn.addEventListener('click', function () {
     console.log('버튼확인');
@@ -20,49 +22,112 @@ myBtn.addEventListener('click', function () {
     location.href = './mycategory.html';
 });
 
-console.log(todolists);
 const main = document.getElementById('main');
-todolists.forEach((todolist) => {
-    const ul = document.createElement('ul');
-    ul.classList.add('main_ul');
+const wrapper = document.querySelector('.wrapper');
 
-    const section = document.createElement('section');
-    const h2 = document.createElement('h2');
+//모달 생성 함수
+const createModal = (todolist) => {
+    const modal = document.createElement('div');
+    modal.classList.add('todo_modal');
 
-    h2.textContent = todolist.title;
-    section.appendChild(h2);
-    ul.appendChild(section);
+    const modalContent = document.createElement('p');
+    modalContent.textContent = '추가할 일을 입력하세요';
+    modalContent.classList.add('modalText');
+    const modalInput = document.createElement('input');
+    modalInput.classList.add('modalInput');
+    const modalBtn = document.createElement('button');
+    modalBtn.classList.add('modalBtn');
+    modalBtn.textContent = '추가';
+    const closeBtn = document.createElement('button');
+    closeBtn.classList.add('closeBtn');
 
-    todolist.todo.forEach((todo) => {
-        const checkLabel = document.createElement('label');
+    modal.appendChild(closeBtn);
+    modal.appendChild(modalContent);
+    modal.appendChild(modalInput);
+    modal.appendChild(modalBtn);
 
-        const checkInput = document.createElement('input');
-        checkInput.type = 'checkbox';
-        checkInput.classList.add('hidden_checkbox');
+    wrapper.appendChild(modal);
 
-        const heartDiv = document.createElement('div');
-        heartDiv.classList.add('show_checkbox');
-
-        const span = document.createElement('span');
-        span.classList.add('todo__span');
-        span.textContent = todo;
-
-        const li = document.createElement('li');
-
-        li.appendChild(checkLabel);
-        checkLabel.appendChild(checkInput);
-        checkLabel.appendChild(heartDiv);
-        checkLabel.appendChild(span);
-
-        li.append(span);
-        ul.appendChild(li);
-
-        checkInput.addEventListener('change', () => {
-            const checkedCount = countTodolist() - document.querySelectorAll('.hidden_checkbox:checked').length;
-            let todoCount = document.querySelector('.todoCount');
-            todoCount.textContent = checkedCount;
-            console.log(`현재 ${checkedCount}개가 체크되었습니다.`);
-        });
+    closeBtn.addEventListener('click', () => {
+        modal.style.display = 'none';
     });
-    main.appendChild(ul);
-});
+
+    modalBtn.addEventListener('click', () => {
+        todolist.push(modalInput.value);
+        relenderTodoList();
+        modal.style.display = 'none';
+    });
+};
+
+//리렌더시 main 데이터 삭제 함수
+const deleteTodoList = () => {
+    while (main.firstChild) {
+        main.removeChild(main.firstChild);
+    }
+};
+
+//리렌더 함수
+const relenderTodoList = () => {
+    deleteTodoList();
+    createTodoList();
+    let todoCount = document.querySelector('.todoCount');
+    todoCount.textContent = countTodolist();
+};
+
+//todoList 렌더함수
+const createTodoList = () => {
+    todolists.forEach((todolist) => {
+        const ul = document.createElement('ul');
+        ul.classList.add('main_ul');
+
+        const section = document.createElement('section');
+        const h2 = document.createElement('h2');
+        const plusImg = document.createElement('img');
+
+        h2.textContent = todolist.title;
+        plusImg.src = './image/plus_button.png';
+        plusImg.classList.add('plusImgBtn');
+        section.appendChild(h2);
+        section.appendChild(plusImg);
+
+        ul.appendChild(section);
+        plusImg.addEventListener('click', () => {
+            createModal(todolist.todo);
+        });
+
+        todolist.todo.forEach((todo) => {
+            const checkLabel = document.createElement('label');
+
+            const checkInput = document.createElement('input');
+            checkInput.type = 'checkbox';
+            checkInput.classList.add('hidden_checkbox');
+
+            const heartDiv = document.createElement('div');
+            heartDiv.classList.add('show_checkbox');
+
+            const span = document.createElement('span');
+            span.classList.add('todo__span');
+            span.textContent = todo;
+
+            const li = document.createElement('li');
+            li.appendChild(checkLabel);
+
+            checkLabel.appendChild(checkInput);
+            checkLabel.appendChild(heartDiv);
+            checkLabel.appendChild(span);
+
+            li.append(span);
+            ul.appendChild(li);
+
+            checkInput.addEventListener('change', () => {
+                const checkedCount = countTodolist() - document.querySelectorAll('.hidden_checkbox:checked').length;
+                let todoCount = document.querySelector('.todoCount');
+                todoCount.textContent = checkedCount;
+                console.log(`현재 ${checkedCount}개의 일이 남았습니다.`);
+            });
+        });
+        main.appendChild(ul);
+    });
+};
+
+createTodoList();
