cc1382766d9002e20d75a9561826d579696e2667
ljh0608 <m98m@catholic.ac.kr>
2023-05-04 00:48:36 +0900

    Feat: 두쌍의 카드배열 함수 구현

--- cardgame_/vite-project/src/utils/doubleArray.js
+++ cardgame_/vite-project/src/utils/doubleArray.js
@@ -1,6 +1,5 @@
 import { DATA } from "../constant/CARD.js";
 
 export const doubleArray = (DATA) => {
-  const datas = DATA.map((data) => data);
-  return datas.concat(datas);
+  return [...DATA, ...DATA];
 };
