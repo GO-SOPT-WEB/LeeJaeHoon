260415bddcdb92c7199ab9fbd5318f24a7285d9b
ljh0608 <m98m@catholic.ac.kr>
2023-05-03 19:00:52 +0900

    Init: nav bar

---
+++ cardgame_/vite-project/src/component/nav/nav.style.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const NavWrapper = styled.nav`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 12rem;
+  gap: 1rem;
+`;
