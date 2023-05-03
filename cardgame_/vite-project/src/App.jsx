daaa5b20bd697a56551d44e2b971f9b579f31fe6
ljh0608 <m98m@catholic.ac.kr>
2023-05-04 01:17:24 +0900

    Refactor: MainSection 컴포넌트화

--- cardgame_/vite-project/src/App.jsx
+++ cardgame_/vite-project/src/App.jsx
@@ -1,19 +1,10 @@
 import { useState } from "react";
-import Card from "./component/card/cardItem";
-import { DATA } from "./constant/CARD";
-import Header from "./component/header/header";
+
+import Header from "./component/header/Header";
+import CardList from "./component/cardList/CardList";
+import Nav from "./component/nav/Nav";
+import Main from "./component/main/Main";
 import "./App.css";
-import styled from "styled-components";
-
-const CardListWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 800px;
-  height: 300px;
-
-  flex-wrap: wrap;
-`;
 
 function App() {
   const [count, setCount] = useState(0);
@@ -21,11 +12,7 @@ function App() {
   return (
     <>
       <Header correct={2} EA={5}></Header>
-      <CardListWrapper>
-        {DATA.map((card) => (
-          <Card key={card.title} src={card.src}></Card>
-        ))}
-      </CardListWrapper>
+      <Main></Main>
     </>
   );
 }
